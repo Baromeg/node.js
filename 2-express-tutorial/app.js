9fcee59ea9b5fbe331c8109eead4d7afe0dce7ca
baromeg <baromeg@gmail.com>
2021-04-11 10:27:49 +0100

    Basic express example

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,29 +1,25 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+const express = require('express')
+const app = express()
 
-// get all files
-const homePage = readFileSync('./index.html')
-
-const server = http.createServer((req, res) => {
-  console.log(req.method)
-  console.log(req.url)
-  if (req.url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
-  }
-  // about page
-  else if (req.url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+// app.get
+app.get('/', (req, res) => {
+  console.log('user hit the resource')
+  res.status(200).send('Home Page')
 })
 
-server.listen(5000)
+app.get('/about', (req, res) => {
+  res.status(200).send('about page')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>resource not found</h1>')
+})
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
+app.listen(5000, () => {
+  console.log('server is listening on port 5000...')
+})
